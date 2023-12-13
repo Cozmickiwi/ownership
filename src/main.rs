7a070b82352db4c473ba363039f6e0921280f4d4
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-13 20:54:52 +1300

    create first word function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let word = String::from("hello world!");
+    let word_s = first_word(&word);
+    println!("{word_s}");
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
 }
